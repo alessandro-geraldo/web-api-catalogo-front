218876122190460e74a5a7af99b0b74805c0a01d
alessandro geraldo <alessandro.com@hotmail.com>
2024-02-26 22:02:12 -0300

    criação do projeto e modulos

---
+++ Catalogo/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { CategoriaDetalheComponent } from './components/categoria-detalhe/categoria-detalhe.component';
+import { CategoriaNovaComponent } from './components/categoria-nova/categoria-nova.component';
+import { CategoriaEditarComponent } from './components/categoria-editar/categoria-editar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriasComponent,
+    CategoriaDetalheComponent,
+    CategoriaNovaComponent,
+    CategoriaEditarComponent,
+    LoginComponent,
+    LogoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
